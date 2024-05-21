111bf83bd1fbaaab6feb8b749708d827053ec8ed
Sophie Allen (as004) <sallen10@student.bbk.ac.uk>
2024-05-21 18:49:41 +0100

    Fingerprint clustering (binary)

--- 05_SCRIPTS/mobile_helix_conservation.py
+++ 05_SCRIPTS/mobile_helix_conservation.py
@@ -80,7 +80,7 @@ for seq in AlignIO.read("./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_t
     id +=1
 
 # create region fingerprint for each sequence
-def region_fingerprint(start, end, region_length, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa"):
+def region_fingerprint(start, end, region_length, average, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa"):
     alignment = AlignIO.read(alignment_path, "fasta")
     insertion_region = list(range(start, end))
     id=0
@@ -92,24 +92,24 @@ def region_fingerprint(start, end, region_length, alignment_path = "./01_DATA/Am
         for pos in insertion_region:
             if seq[pos-1] != '-':
                 seq_occupancy += 1
-        # if occupancy more than 50%, consider species to have the insertion, add to fingerprint
-        if seq_occupancy/region_length >= 0.5:
+        # if occupancy more than average, consider species to have the insertion, add to fingerprint
+        if seq_occupancy/region_length >= average:
             #sufficient occupancy in alignment: insertion region present
             fingerprints[unique_key].append(1)
-        elif seq_occupancy/region_length < 0.5:
+        elif seq_occupancy/region_length < average:
             #not enough occupancy: no insertion region
             fingerprints[unique_key].append(0)
         id+=1
 
 print ("Creating fingerprinting dictionary...")
-region_fingerprint(1, 6, 6, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-1
-region_fingerprint(15, 57, 43, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-2
-region_fingerprint(81, 85, 5, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-3
-region_fingerprint(96, 128, 33, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-4
-region_fingerprint(154, 175, 22, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-5
-region_fingerprint(184, 241, 58, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-6
-region_fingerprint(252, 303, 52, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-7
-region_fingerprint(355, 399, 45, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-8
+region_fingerprint(1, 6, 6, 1.17, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-1
+region_fingerprint(15, 57, 43, 12.9, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-2
+region_fingerprint(81, 85, 5, 1.3, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-3
+region_fingerprint(96, 128, 33, 5.3, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-4
+region_fingerprint(154, 175, 22, 6.1, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-5
+region_fingerprint(184, 241, 58, 24.0, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-6
+region_fingerprint(252, 303, 52, 25.4, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-7
+region_fingerprint(355, 399, 45, 6.1, alignment_path = "./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa") #I-8
 
 # this should give a dictionary of 20304 key-value pairs, where each key is the species and each value is a list of 8 0/1 values 
 # where 0 indicates no insertion, and 1 indicates the insertion is present
@@ -118,16 +118,26 @@ with open ("./01_DATA/Amidase_3/05_1_Region_Conservation/fingerprints.json", "w"
     json.dump(fingerprints, file)
 print ("Fingerprints saved!")
 
-# to go here: code that clusters species into groups based on having the same binary fingerprint
+# cluster species into groups based on having the same binary fingerprint
 ## re-reading the dictionary
 import json
 with open("./01_DATA/Amidase_3/05_1_Region_Conservation/fingerprints.json", "r") as file:
     fingerprints = json.load(file)
-    print(fingerprints)
 
-# Phylogeny
+# generate filtered dictionaries for each possible cluster of insertions
+import itertools
+length = 8
+options = list(itertools.product([0, 1], repeat=length))
+options_list = [list(option) for option in options]
+for option in options_list:
+    cluster = {key: value for key, value in fingerprints.items() if value == option}
+    if len(cluster) != 0:
+        with open (f"./01_DATA/Amidase_3/05_1_Region_Conservation/{option}", "w") as f:
+            json.dump(cluster, f)
+
+
+# Phylogeny prep
 alignment=AlignIO.read("./01_DATA/Amidase_3/04_Multiple_Alignment/alignment_3_thresh1.0.fa","fasta")
-
 unique_names = set()
 
 for i, record in enumerate(alignment):
@@ -138,7 +148,7 @@ for i, record in enumerate(alignment):
     if truncated_name in unique_names:
         truncated_name = f"{truncated_name[:4]}_{i}"
     unique_names.add(truncated_name)
-    
+
     record.id=truncated_name
 
 # Write the new alignment
